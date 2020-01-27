86173df39eec5559d910259c06eb59b77e5218b0
Igor <igor1215@gmail.com>
2020-01-27 15:03:15 -0330

    csr2

--- simulation/csrdictionary.cpp
+++ simulation/csrdictionary.cpp
@@ -1,8 +1,10 @@
-#include "csrdictionary.h"
 #include <algorithm>
+#include <stdexcept>
+#include "csrdictionary.h"
 
 icy::CSRDictionary::CSRDictionary()
 {
+    // these are typical values for medium-sized FEM geometry
     int initial_size = 50000;
     int reserve = 100000;
 
@@ -13,8 +15,6 @@ icy::CSRDictionary::CSRDictionary()
     rows_dynamicNeighbors.resize(initial_size);
 
     rows_pcsr.reserve(reserve);
-    rows_pcsr.resize(initial_size);
-
     rows_sortedAllNeighbors.reserve(reserve);
 
     csr_cols = csr_rows = nullptr;
@@ -65,7 +65,7 @@ void icy::CSRDictionary::CreateStructure()
         rows_pcsr[i].clear();
         std::vector<int> & sortedAllNeighbors = rows_sortedAllNeighbors[i];
         sortedAllNeighbors.clear();
-        sortedAllNeighbors.reserve(30);
+        sortedAllNeighbors.reserve(30); // typical value (maybe overkill) for tetrahedral meshes
 
         std::set<int> &staticNeighbors = rows_staticNeighbors[i];
         std::set<int> &dynamicNeighbors = rows_dynamicNeighbors[i];
@@ -79,7 +79,32 @@ void icy::CSRDictionary::CreateStructure()
     nnz = 0;
     for(auto const &vec : rows_sortedAllNeighbors) nnz+=vec.size();
 
+    // allocate structure arrays
+    if(csr_rows_size < N+1) {
+        csr_rows_size = N+1;
+        delete csr_rows;
+        csr_rows = new int[csr_rows_size];
+    }
 
+    if(csr_cols_size < nnz) {
+        csr_cols_size = nnz*2;
+        delete csr_cols;
+        csr_cols = new int[csr_cols_size];
+    }
+
+    csr_rows[N] = nnz;
+
+    // enumerate entries
+    for(int i=0,count=0;i<N;i++)
+    {
+        csr_rows[i] = count;
+        for(int const &local_column : rows_sortedAllNeighbors[i])
+        {
+            rows_pcsr[i][local_column] = count;
+            csr_cols[count] = local_column;
+            count++;
+        }
+    }
 }
 
 int icy::CSRDictionary::offset(int row, int column)
@@ -87,7 +112,18 @@ int icy::CSRDictionary::offset(int row, int column)
     return rows_pcsr[row][column];
 }
 
-void icy::CSRDictionary::Assert(bool nonSymmetric)
+void icy::CSRDictionary::Assert()
 {
+    if(csr_rows[0] != 0) throw std::runtime_error("rows[0] != 0");
+    if(csr_rows[N] != nnz) throw std::runtime_error("rows[N] != nnz");
+    for (int i = 1; i < N + 1; i++)
+        if (csr_rows[i] <= csr_rows[i - 1]) throw std::runtime_error("rows[i] is not increasing");
 
+    // verify columns array, upper triangular
+    for (int i = 0; i < N; i++)
+    {
+        if (csr_cols[csr_rows[i]] != i) throw std::runtime_error("structure not UT");
+        for (int j = csr_rows[i]; j < csr_rows[i + 1] - 1; j++)
+            if (csr_cols[j + 1] <= csr_cols[j]) throw std::runtime_error("cols in same row not increasing");
+    }
 }
--- simulation/csrdictionary.h
+++ simulation/csrdictionary.h
@@ -28,13 +28,15 @@ public:
     void AddDynamic(int row, int column);   // insert dynamic non-zero element
     void AddStatic(int row, int column);
     void CreateStructure();
-    int offset(int row, int column);    // returns the id of an i-j pair (pairs are consecutively numbered)
-    void Assert(bool nonSymmetric); // check matrix consistency
+    int offset(int row, int column);        // returns the id of an i-j pair (pairs are consecutively numbered)
+    void Assert();                          // check matrix consistency
 
 private:
     int maxRowIndex = -1;   // maximum row index, zero-based, equal to resulting matrix height minus 1
     std::vector<std::map<int,int>> rows_pcsr;   // per row mappings between columns and offset in "values"
     std::vector<std::vector<int>> rows_sortedAllNeighbors;
+    int csr_rows_size = 0;
+    int csr_cols_size = 0;
 };
 
 #endif // CSRDICTIONARY_H
