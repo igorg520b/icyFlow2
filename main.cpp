9e05621d23d3e4435bda3f4619178fe51d69fca1
Igor Gribanov <igor1215@gmail.com>
2020-03-06 16:30:36 -0330

    no gui

--- main.cpp
+++ main.cpp
@@ -2,18 +2,37 @@
 
 #include <QApplication>
 #include <QSurfaceFormat>
-
+#include <QCommandLineParser>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QApplication::setApplicationName("icFlow");
+    QApplication::setApplicationVersion("2.1");
 
     QSurfaceFormat fmt = QVTKOpenGLNativeWidget::defaultFormat();
     fmt.setAlphaBufferSize(0);
     QSurfaceFormat::setDefaultFormat(fmt);
-
     MainWindow w;
 
-    w.showMaximized();
+    // parse command line options
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addVersionOption();
+    // An option with a value
+    QCommandLineOption fpbOption(QStringList() << "b" << "four-point-bending",
+            QCoreApplication::translate("main", "Model four-point beam bending with index parameter <idx>"),
+            QCoreApplication::translate("main", "idx"));
+    parser.addOption(fpbOption);
+    parser.process(a);
+
+    if(parser.isSet(fpbOption)) {
+        qDebug() << "no-GUI mode";
+        QString fpbIndexValue = parser.value(fpbOption);
+        qDebug() << fpbIndexValue;
+    } else {
+        w.showMaximized();
+    }
     return a.exec();
 }
